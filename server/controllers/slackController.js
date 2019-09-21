c501d9b8d36f16ae7d65f0c02cf49fd1eff2adfd
Adam Stover <nehtaro@gmail.com>
2019-09-20 20:22:07 -0700

    first slack Controller

---
+++ server/controllers/slackController.js
@@ -0,0 +1,22 @@
+const slackController = { };
+
+slackController.getGeneralHistory = async (req, res, next) => {
+  console.log('slackController.getGeneralHistory');
+  const channel = 'CKA6RDALE';
+  const latest = Math.floor(Date.now() / 1000);
+  const oldest = latest - 86400;
+  const URI = `https://slack.com/api/conversations.history?token=${process.env.API_KEY}&channel=${channel}&latest=${latest}&oldest=${oldest}`;
+  const rawResult = await fetch(URI, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  const parsedResult = await rawResult.json();
+  console.log(parsedResult);
+  next();
+};
+
+process.env.API_KEY
+
+module.exports = slackController;
