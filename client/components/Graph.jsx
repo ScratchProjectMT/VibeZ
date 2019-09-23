6e41f9a0463bacc2d6d14a6ee61eb2d669e12cd8
jasonlee1245 <jasonlee1245@gmail.com>
2019-09-23 12:51:26 -0700

    finished

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Bar, Line } from 'react-chartjs-2';
 
 const Graph = (props) => {
-  const {graph, graphType, data} = props;
+  const {graph, graphType, data} = props; //deconstruct the props
   let graphData;
   let options;
+  //we don't want to run this stuff when data hasn't loaded yet from first render
   if(props.data) {
-    const xAxis = [];
-    const yAxis = [];
-    const messages = [];
-    const reactions = [];
+    const xAxis = []; //the time and date 
+    const yAxis = []; //the sentiment analysis of the messages
+    const messages = []; //the messages itself
+    const reactions = []; //the sentiment analysis of the reaction emotes
+    //this is to help show the messages when you hover over the points on the graph
     options = { 
       maintainAspectRatio: false,
-      tooltips: {
+      tooltips: { //tooltips are the black boxes that appear when you hover over a vertex
         callbacks: {
           afterLabel: function(tooltipItem, data) {
             return data.datasets[tooltipItem.datasetIndex].msg[tooltipItem.index]
@@ -21,8 +23,9 @@ const Graph = (props) => {
       }
     }
     //colors:  green: (24, 166, 137, 0.6) red: (255, 99, 132, 0.6) 
-    //         blue: (3, 0, 129, 0.6) yellow: (255, 211, 1, 0.6)
+    //helper   blue: (3, 0, 129, 0.6) yellow: (255, 211, 1, 0.6)
     for(let i = data.length-1; i >= 0; i--) {
+      //configure time settings
       let newTime = new Date(data[i].time * 1000);
       newTime = newTime.toISOString().slice(0,16);
       xAxis.push(newTime);
@@ -38,7 +41,7 @@ const Graph = (props) => {
           data: yAxis,
           backgroundColor: 'rgba(255, 99, 132, 0.6)',
           borderColor:'rgba(255, 99, 132, 0.6)',
-          msg: messages,
+          msg: messages,  //custom key for messages
           fill: false
         },
         {
@@ -47,12 +50,13 @@ const Graph = (props) => {
           backgroundColor: 'rgba(24, 166, 137, 0.6)',
           borderColor: 'rgba(24, 166, 137, 0.6)',
           hidden: true,
-          msg: messages,
+          msg: messages, //custom key for messages
           fill: false
         }
       ]
     }
   }
+  //conditional rendering for different graphs
   if(graph && graphType === 'Line Graph') {
     return (
       <div className='chart'>
